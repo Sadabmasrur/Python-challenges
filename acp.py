d1ac6c36d75d6329a8d959c962c9e13e2481a77a
Sadab Masrur <sadabmasrur08@gmail.com>
2025-03-06 17:41:44 +0600

    acp

---
+++ acp.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+def randPass(length):
+    if length<6:
+        return "The password should be at least 6 characters"
+
+    lowercases=string.ascii_lowercase
+    uppercases=string.ascii_uppercase
+    digits=string.digits
+    punctuations=string.punctuation
+
+    password=[random.choice(lowercases),random.choice(uppercases),random.choice(digits),random.choice(punctuations)]
+    characters=lowercases+uppercases+digits+punctuations
+    password=password+random.choices(characters,k=length-len(password))
+    random.shuffle(password)
+
+    return "".join(password)
+
+passLength=random.randint(6,10)
+password=randPass(passLength)
+
+print(f"Password: {password}")
